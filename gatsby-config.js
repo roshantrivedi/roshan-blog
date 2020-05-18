0387d97ec324cc3061ceb83fa897c1a2c643a84b
roshantrivedi <42265351+roshantrivedi@users.noreply.github.com>
2020-05-19 02:24:41 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Roshan Trivedi`,
+      summary: `who lives and Mysore in India learning new things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://roshantrivedi.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
